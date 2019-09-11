887214b9e4080db8895330f9944414438782df72
Eunice Son <eson@ESON-011019.local>
2019-09-11 18:23:25 -0500

    nativeLayers array objects selected

--- src/reverse-selected.js
+++ src/reverse-selected.js
@@ -1,18 +1,39 @@
 const sketch = require('sketch')
 const document = sketch.getSelectedDocument();
-const selectedLayers = document.selectedLayers;
+const selectedPage = document.selectedPage;
+
+const nativeLayers = selectedPage.sketchObject.children();
+
+const wrappedLayerArray = new Array();
+
+nativeLayers.forEach(nativelayer => {
+    const layer = sketch.fromNative(nativelayer);
+    if(layer.selected == true && layer.type != 'Page')
+    {
+        wrappedLayerArray.push(layer);
+    }
+    }
+);
+console.log(wrappedLayerArray)
+wrappedLayerArray.reverse();
+console.log(wrappedLayerArray)
+
+
+
+
 
 // for (var i = 0; i < selectedLayers.length; i++) {
 //     const layer = sketch.fromNative(selectedLayers[i]);
 //     console.log(layer.index);
 // }
 
-selectedLayers.forEach((nativelayer) => {
-    const layer = sketch.fromNative(nativelayer);
-    for (var i = selectedLayers.length - 1; i > 0; i--) {
-        layer.moveForward();
-    }
-});
+// selectedLayers.forEach((nativelayer) => {
+//     const layer = sketch.fromNative(nativelayer);
+//     for (var i = selectedLayers.length - 1; i > 0; i--) {
+//        // layer.moveForward();
+//        console.log(layer);
+//     }
+// });
 
 // for (var i = 0; i < selectedLayers.length; i++) {
 //     var layer = sketch.fromNative(selectedLayers[i]);
