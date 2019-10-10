691b5fe7765c7ca4df711cbc8495b85fdee9fd24
Eunice Son <eson@ESON-011019.local>
2019-10-10 12:43:00 -0500

    reverse layers with consecutive layers message

--- src/reverse-selected.js
+++ src/reverse-selected.js
@@ -8,7 +8,17 @@ selectedLayers.forEach(nativeLayer => {
     indexArray.push(layer.index);
 });
 
+for (var i = 1; i < indexArray.length; i++) {
+    if (indexArray[i] !== indexArray[i-1]+1) {
+        const message="Please select consecutive layers 🙅🏻";
+        sketch.UI.message(message);
+        throw new Error(message);
+    }
+}
+
 selectedLayers.forEach(nativeLayer => {
     var layer = sketch.fromNative(nativeLayer);
     layer.index = indexArray.pop();
 });
+
+sketch.UI.message("You've successsfully reversed your layers 💁🏻")
